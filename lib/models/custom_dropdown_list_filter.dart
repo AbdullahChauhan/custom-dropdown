0e2f9adafeb3f6695e444b28580a6909e852cd74
AbdullahChauhan <acse.uok@gmail.com>
2023-12-30 19:47:30 +0500

    Minor change

--- lib/models/custom_dropdown_list_filter.dart
+++ lib/models/custom_dropdown_list_filter.dart
@@ -3,5 +3,6 @@ part of '../custom_dropdown.dart';
 mixin CustomDropdownListFilter {
   /// Used to filter elements against query on
   /// [CustomDropdown<T>.search] or [CustomDropdown<T>.searchRequest]
+  /// [CustomDropdown<T>.multiSelect] or [CustomDropdown<T>.multiSelectSearchRequest]
   bool filter(String query);
 }
